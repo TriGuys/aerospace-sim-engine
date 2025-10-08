50dd1f5fa2a21e6c54385ef72bd0634976f648ee
Dom Brown <dominicbrown4@hotmail.com>
2025-10-08 10:52:01 +0100

    add fault class attributes

--- FaultDetection.py
+++ FaultDetection.py
@@ -1,6 +1,10 @@
 class Fault():
-    def __init__(self):
-        pass
+    def __init__(self, faultID, severity, description, timestamp, status):
+        self.faultID = faultID
+        self.severity = severity
+        self.description = description
+        self.timestamp = timestamp
+        self.status = status
 
 class FaultDetection():
     def __init__(self):
