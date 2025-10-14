047df4c629ead501466817f857f83a2926d36066
Flinn Mort <flinnmort13@outlook.com>
2025-10-14 21:08:10 +0100

    Create database alert table initilisation.

--- Database.py
+++ Database.py
@@ -8,10 +8,24 @@ class db:
 
     def __init__(self, db_path: str = "alerts.db") -> None:
         self.db_path = db_path
-        Path(self.db_path).parent.mkdir(parents=True, exist_ok=True)
+        Path(self.db_path).parent.mkdir(parents=True, exist_ok=True) # make database file if it doesn't exist
         self._init_table()
 
     def _connect(self) -> sqlite3.Connection:
         con = sqlite3.connect(self.db_path)
         con.row_factory = sqlite3.Row
         return con
+    
+    def _init_table(self) -> None:
+        with self._connect() as con:
+            con.execute(
+                """
+                CREATE TABLE IF NOT EXISTS alerts (
+                  id              INTEGER PRIMARY KEY AUTOINCREMENT,
+                  source          TEXT    NOT NULL,
+                  severity        TEXT    NOT NULL,
+                  message         TEXT    NOT NULL,
+                  created_at      TEXT    NOT NULL DEFAULT (STRFTIME('%Y-%m-%dT%H:%M','now'))
+                )
+                """
+            )
