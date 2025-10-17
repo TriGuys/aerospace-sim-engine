7020962a3570f45d407bd8c4fedb814ff8b10cf9
Flinn Mort <flinnmort13@outlook.com>
2025-10-17 09:12:14 +0100

    Implement rough abstract functionality for create alert function

--- AlertModule.py
+++ AlertModule.py
@@ -5,5 +5,19 @@ class AlertModule():
     def __init__(self):
         pass
 
-    def alert(self, message:str):
+    def create_alert(self, sensor_id, fault_code, severity, message, timestamp):
+        """
+        intialise alert creation dataclass
+        create_alert = AlertCreation(
+            sensor_id = sensor_id
+            fault_code = fault_code
+            severity = severity
+            message = message
+            timestamp = timestamp
+        )
+
+        display alert on user interface table
+
+        """
+
         pass
