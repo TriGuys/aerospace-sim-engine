92734ef2ff7987750bc273d4a17fb8b13dfc1e57
KMarshall33 <30208892@blackpool.ac.uk>
2025-10-17 17:02:43 +0100

    Added sensor module interfaces to Abstractions.py

--- Abstractions.py
+++ Abstractions.py
@@ -1,5 +1,43 @@
 from dataclasses import dataclass
 
+from __future__ import annotations
+from abc import ABC, abstractmethod
+from typing import Protocol
+import pandas as pd
+
+class Reader(ABC):
+    @abstractmethod
+    def read(self) -> pd.DataFrame:
+        raise NotImplementedError
+    
+class Validator(ABC):
+    @abstractmethod
+    def validate(self, df: pd.DataFrame) -> pd.DataFrame:
+        raise NotImplementedError
+    
+class Preprocessor(ABC):
+    @abstractmethod
+    def preprocess(self, df: pd.DataFrame) -> pd.DataFrame:
+        raise NotImplementedError
+
+class OutlierDetector(ABC):
+    @abstractmethod
+    def detect(self, df: pd.DataFrame) -> pd.DataFrame:
+        raise NotImplementedError
+    
+class Pipeline(Protocol):
+    def run(self) -> pd.DataFrame: ...
+    @property
+    def data(self) -> pd.DataFrame: ...
+
+__all__ = [
+    "Reader",  
+    "Validator",
+    "Preprocessor",
+    "OutlierDetector",
+    "Pipeline",
+]
+
 @dataclass(frozen=True)
 class AlertCreation:
     sensor_id: str
