ee584b57291b410c94d1e58367215bbb94fa894d
KMarshall33 <30208892@blackpool.ac.uk>
2025-10-22 09:57:43 +0100

    Add initial functionality to read and convert csv files to DataFrame.

--- SensorIntegration.py
+++ SensorIntegration.py
@@ -1,3 +1,6 @@
+import pandas as pd
+from pathlib import Path
+import logging
 
 class SensorIntegration():
 
@@ -6,3 +9,18 @@ class SensorIntegration():
 
     def sensor(self):
         pass
+
+    def read_csv(self, file_path: str) -> pd.DataFrame:
+        file = Path(file_path)
+        if not file.exists():
+            raise FileNotFoundError(f"File not found: {file_path}")
+        logging.info(f"Loading sensor data from: {file_path}")
+        df = pd.read_csv(file)
+
+        self.validate_data(df)
+        df = self.clean_data(df)
+
+        self.data=(df)
+        logging.info(f"Sensor data loaded successfully with: {len(df)} records.")
+        return df
+    
