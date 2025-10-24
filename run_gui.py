01bad9a71fad82f1de026308eca85864fa8d1a69
Dom Brown <dominicbrown4@hotmail.com>
2025-10-24 13:07:36 +0100

    remove run_gui.py test file from branch

--- run_gui.py
+++
@@ -1,8 +0,0 @@
-import tkinter as tk
-from UserInterface import UserInterface
-
-if __name__ == "__main__":
-    root = tk.Tk()
-    ui = UserInterface(root)
-    ui.draw_window()
-    root.mainloop()
